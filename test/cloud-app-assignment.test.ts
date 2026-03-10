6f35dcb81aa467ef2d19df2ed0bdfc29290b2f21
Sicheng Mu <musicheng520@gmail.com>
2026-03-10 22:37:53 +0000

    Initial commit

---
+++ test/cloud-app-assignment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudAppAssignment from '../lib/cloud-app-assignment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-app-assignment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudAppAssignment.CloudAppAssignmentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
